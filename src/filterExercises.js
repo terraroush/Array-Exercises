772d3c7d3c8b4d928b71003a6698703ee16457d2
Terra Roush <terra.roush@gmail.com>
2020-09-30 14:06:12 -0500

    passed first filter problem

--- src/filterExercises.js
+++ src/filterExercises.js
@@ -7,22 +7,46 @@ const instructors = useInstructors();
 // It should accept one integer parameter named `cohort`
 // It should return an array of just the students who are in that cohort
 
+ export const getStudentsInCohort = (cohort) => {
+    let specificCohort = students.filter(student => student.cohort === cohort)
+    return specificCohort
+}
+
 // Export a function called getFullTimeStudents
 // It should not accept any parameters
 // It should return an array of only the full time students
 
+// export const getFullTimeStudents = () => {
+//     const fullTimers = students.filter(student => student.fullTime === true)
+//     return fullTimers
+// }
+
 // Export a function called getStudentsByInstructorId
 // It should accept one integer parameter name `instructorId`
 // It should return an array of students with that instructor
+
+// export const getStudentsByInstructorId = (instructorId) => {
+//     const taughtBy = students.filter(student => student.instructorId === instructorId)
+//     return taughtBy
+// }
 
 // Export a function called getPolyglotStudents
 // It should accept one integer parameter named `languageCount`
 // It should return an array of students who know as many (or more) languages than `languageCount`
 // Ex: If the number 2 is passed to the function, only the students who know 2 or more languages should be returned
 
+// export const getPolyglotStudents = (languageCount) => {
+//     const polyglottons = students.filter(student => student.languages >= 2) 
+//     return polyglottons
+// }
+
 // Export a function called getAvailableInstructors
 // It should not accept any parameters
 // It should return an array of instructors that don't have any students
+
+// export const getAvailableInstructors = () => {
+//     const studentless = students.filter()
+// }
 
 // Export a function called getStudentsByLanguage
 // It should accept one string parameter named `language`
