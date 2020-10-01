9c854a9763d18c0f5246db8e64ec158ab7b9135a
Terra Roush <terra.roush@gmail.com>
2020-09-30 20:00:44 -0500

    passed second find problem

--- src/findExercises.js
+++ src/findExercises.js
@@ -19,11 +19,19 @@ export const getStudentById = (id) => {
 // It should return the instructor object with the matching ID
 // Ex: getInstructorById(1)
 
+export const getInstructorById = id => {
+    const instructorById = instructors.find(instructor => instructor.id === id)
+    return instructorById
+}
+
 // Export a function called getStudentByLastName
 // It should accept one string parameter named `lastName`
 // It should return the student object whose last name matches `lastName`
 // It should NOT be case sensitive
 // Ex: getStudentByName("sMiTh")
+// export const getStudentByLastName = ("lastName") => {
+
+// }
 
 // Export a function called getStudentByName
 // It should accept one string parameter named `fullName`
